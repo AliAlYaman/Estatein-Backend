a97b042e8ebabe0e02ae2a276aa75e93402881e7
Ali Al Yaman <alialyaman750@gmail.com>
2024-10-05 21:28:32 +0300

    Update PropertyController.php

--- Estatein/app/Http/Controllers/Api/V1_0/PropertyController.php
+++ Estatein/app/Http/Controllers/Api/V1_0/PropertyController.php
@@ -11,4 +11,59 @@ class PropertyController extends Controller
     public function index(){
         return response()->json(Property::all());
     }
+
+    public function filter(Request $request)
+    {
+        // Retrieve the input parameters, which might be null
+        $searchTerm = $request->input('searchTerm');
+        $location = $request->input('location');
+        $propertyType = $request->input('propertyType');
+        $pricingRange = $request->input('pricingRange');
+        $propertySize = $request->input('propertySize');
+        $buildYear = $request->input('buildYear');
+
+        // Start the query for the Property model
+        $query = Property::query();
+
+        // Conditionally add where clauses only if the parameters are not null
+        if ($searchTerm) {
+            $query->where('title', 'like', '%' . $searchTerm . '%');
+        }
+
+        if ($location) {
+            $query->where('location', $location);
+        }
+
+        if ($propertyType) {
+            $query->where('type', $propertyType);
+        }
+
+        if ($pricingRange) {
+            $range = explode('-', $pricingRange);
+            if (count($range) == 2) {
+                $query->whereBetween('price', [(int)$range[0], (int)$range[1]]);
+            }
+        }
+
+        if ($propertySize) {
+            $query->where('size', '>=', $propertySize);
+        }
+
+        if ($buildYear) {
+            // Check if the input is a range like "2010-2020"
+            $yearRange = explode('-', $buildYear);
+            if (count($yearRange) == 2) {
+                $query->whereBetween('build_year', [(int)$yearRange[0], (int)$yearRange[1]]);
+            } else {
+                // If it's not a range, assume it's a single year
+                $query->whereYear('build_year', $buildYear);
+            }
+        }
+
+        // Execute the query and get the results
+        $properties = $query->get();
+
+        // Return the results as a JSON response
+        return response()->json($properties);
+    }
 }
